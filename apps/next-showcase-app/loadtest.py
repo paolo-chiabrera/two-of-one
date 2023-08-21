ed07424f155ef6d9e794408ad1b07790aa75faa8
Claudia Ballano <claudia.ballano@glovoapp.com>
2023-08-21 14:26:58 +0200

    feat(apps): load test

---
+++ apps/next-showcase-app/loadtest.py
@@ -0,0 +1,14 @@
+from locust import HttpUser, TaskSet, task, constant, LoadTestShape
+import json
+import os
+
+class MyUser(HttpUser):
+    host = "https://next.sillyapps.io"
+
+    @task
+    def my_task(self):
+        with self.client.get("/load-test", catch_response=True) as resp:
+            if (resp.status_code == 200):
+                resp.success()
+            else:
+                resp.failure("bad response " + str(resp.status_code) + "==" + str(resp.headers))
