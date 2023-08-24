39c9edfcde2d04f4a7e12316d93c1130ebfe0591
Clàudia Ballano <74602061+claudiaballano@users.noreply.github.com>
2023-08-24 18:07:42 +0200

    feat: dynamic cache (#36)

--- apps/next-showcase-app/src/app/page.tsx
+++ apps/next-showcase-app/src/app/page.tsx
@@ -1,9 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
 import StoreTile from "./components/store";
 import FiltersSection from "./components/filters";
 import { RestaurantsReponse, Restaurant } from "./types/restaurants";
-export default async function Home() {
-  const restaurants: RestaurantsReponse = await getRestaurants();
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string };
+}) {
+  const queryString = new URLSearchParams(searchParams);
+  const restaurants: RestaurantsReponse = await getRestaurants(queryString);
 
   const restaurantsList = restaurants.elements.filter(
     ({ type, singleData }: Restaurant) =>
@@ -12,7 +17,7 @@ export default async function Home() {
 
   return (
     <section className="content">
-      <h2>Stores list</h2>
+      <h2>Stores list {queryString}</h2>
       <FiltersSection />
       <section className="stores" id="stores-list">
         {restaurantsList.map((restaurant) => (
@@ -23,17 +28,15 @@ export default async function Home() {
   );
 }
 
-async function getRestaurants() {
+async function getRestaurants(queryString: URLSearchParams) {
+  const restaurantsUrl = `https://comida.sillyapps.io/restaurants.json?${queryString}`;
+  const countriesUrl = `https://comida.sillyapps.io/countries.json?${queryString}`;
+  const citiesUrl = `https://comida.sillyapps.io/cities.json?${queryString}`;
+
   const [restaurants] = await Promise.all([
-    fetch("https://comida.sillyapps.io/restaurants.json", {
-      cache: "no-store",
-    }),
-    fetch("https://comida.sillyapps.io/countries.json", {
-      cache: "no-store",
-    }),
-    fetch("https://comida.sillyapps.io/cities.json", {
-      cache: "no-store",
-    }),
+    fetch(restaurantsUrl),
+    fetch(countriesUrl),
+    fetch(citiesUrl),
   ]);
 
   if (!restaurants.ok) {
