47f70c49f5433d4eb270c768305a645dfb6eaaf9
vd3v <viktor.borisov@glovoapp.com>
2023-08-29 17:13:19 +0200

    feat: use client hydration

--- apps/nuxt-showcase-app/components/StoreList.vue
+++ apps/nuxt-showcase-app/components/StoreList.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>Stores list {{queryParams}}</h2>
+  <h2>Stores list {{ queryParams }}</h2>
   <AppFilters/>
   <section class="stores" id="stores-list">
     <store-tile v-for="restaurant in restaurantsList" :restaurant="restaurant"/>
@@ -11,9 +11,8 @@ import AppFilters from "~/components/AppFilters.vue";
 import StoreTile from "~/components/StoreTile.vue";
 import {RestaurantesReponse} from "~/types/RestaurantesResponse";
 
-const props = defineProps({q:String})
+const props = defineProps({q: String})
 const queryParams = props.q
-
 
 
 const {data, pending, error, refresh} = await useAsyncData(
@@ -24,17 +23,17 @@ const {data, pending, error, refresh} = await useAsyncData(
           $fetch('https://comida.sillyapps.io/countries.json' + queryParams),
           $fetch('https://comida.sillyapps.io/cities.json' + queryParams),
         ]
-    ).then(responses => responses[0])
+    ).then(responses => responses[0].elements.filter(
+        element => {
+          if (element.type !== 'SINGLE') return false;
+          if (element.singleData?.type !== 'STORE') return false;
+          if (!element.singleData?.storeData) return false;
+          return true
+        }
+    ))
 )
 
-const restaurantsList = ((unref(data)) as RestaurantesReponse).elements.filter(
-    element => {
-      if (element.type !== 'SINGLE') return false;
-      if (element.singleData?.type !== 'STORE') return false;
-      if (!element.singleData?.storeData) return false;
-      return true
-    }
-)
+const restaurantsList = ((unref(data)) as RestaurantesReponse)
 
 
 </script>
