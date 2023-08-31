26fb034663c0c803f833c4dc04cb0c4925352c76
Clàudia Ballano <74602061+claudiaballano@users.noreply.github.com>
2023-08-31 16:23:59 +0200

    Revert "feat: use server->client hydration (#47)" (#48)
    This reverts commit 3b1b4b73e95c85a3c223fa54d5423f0c318c9971.

--- apps/nuxt-showcase-app/components/StoreList.server.vue
+++ apps/nuxt-showcase-app/components/StoreList.server.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>Stores list {{ queryParams }}</h2>
+  <h2>Stores list {{queryParams}}</h2>
   <AppFilters/>
   <section class="stores" id="stores-list">
     <store-tile v-for="restaurant in restaurantsList" :restaurant="restaurant"/>
@@ -11,8 +11,9 @@ import AppFilters from "~/components/AppFilters.vue";
 import StoreTile from "~/components/StoreTile.vue";
 import {RestaurantesReponse} from "~/types/RestaurantesResponse";
 
-const props = defineProps({q: String})
+const props = defineProps({q:String})
 const queryParams = props.q
+
 
 
 const {data, pending, error, refresh} = await useAsyncData(
@@ -23,17 +24,17 @@ const {data, pending, error, refresh} = await useAsyncData(
           $fetch('https://comida.sillyapps.io/countries.json' + queryParams),
           $fetch('https://comida.sillyapps.io/cities.json' + queryParams),
         ]
-    ).then(responses => responses[0].elements.filter(
-        element => {
-          if (element.type !== 'SINGLE') return false;
-          if (element.singleData?.type !== 'STORE') return false;
-          if (!element.singleData?.storeData) return false;
-          return true
-        }
-    ))
+    ).then(responses => responses[0])
 )
 
-const restaurantsList = ((unref(data)) as RestaurantesReponse)
+const restaurantsList = ((unref(data)) as RestaurantesReponse).elements.filter(
+    element => {
+      if (element.type !== 'SINGLE') return false;
+      if (element.singleData?.type !== 'STORE') return false;
+      if (!element.singleData?.storeData) return false;
+      return true
+    }
+)
 
 
 </script>
